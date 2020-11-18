24e1bbdecb4e9259340200b628ecb3433c5453ae
Cecilia <cecigallardo@gmail.com>
2020-11-17 21:11:47 -0300

    se hizo una pequeña modificacion en modelos

--- apps/programa/models.py
+++ apps/programa/models.py
@@ -1,36 +1,30 @@
 from datetime import date, datetime
 from django.db import models
 
-
-# Create your models here.
+from apps.core.models import ModeloBase
 from apps.persona.models import Persona
 
 
-class TipoAsistencia(models.Model):
+class TipoAsistencia(ModeloBase):
     descripcion = models.CharField(max_length=150,
                                    unique=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.descripcion
 
 
-class Programa(models.Model):
+class Programa(ModeloBase):
     nombre = models.CharField(max_length=100, unique=True)
     tipo_asistencias = models.ManyToManyField(TipoAsistencia)
-    requisitos = models.FileField(upload_to="requisitos",
-                                  blank=True)
+    requisitos = models.FileField(upload_to="requisitos", blank=True)
     fecha_inicio = models.DateField(default=date.today)
     fecha_fin = models.DateField(null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nombre
 
 
-class AsignacionBeneficio(models.Model):
+class AsignacionBeneficio(ModeloBase):
     programa = models.ForeignKey(Programa, on_delete=models.CASCADE)
     persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
     tipo_asistencia = models.ForeignKey(TipoAsistencia, on_delete=models.CASCADE)
