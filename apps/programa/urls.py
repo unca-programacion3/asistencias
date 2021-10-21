1e2e0222d6002071d735b6224d56d913c19ac064
Cecilia <cecigallardo@gmail.com>
2021-10-20 21:21:21 -0300

    Creacion de vistas basadas en clases

--- apps/programa/urls.py
+++ apps/programa/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import programa_lista, programa_detalle, programa_create, programa_edit, programa_delete
-
+from .views import programa_detalle, programa_delete, ProgramaCreateView, ProgramaListaView, ProgramaUpdateView
 
 app_name = 'programa'
 urlpatterns = [
     # programa views
-    path('', programa_lista, name='programa_lista'),
+    path('', ProgramaListaView.as_view(), name='programa_lista'),
     path('<int:pk>/', programa_detalle, name='programa_detalle'),
-    path('create/', programa_create, name='programa_create'),
-    path('edit/<int:pk>', programa_edit, name='programa_edit'),
+    path('create/', ProgramaCreateView.as_view(), name='programa_create'),
+    path('edit/<int:pk>', ProgramaUpdateView.as_view(), name='programa_edit'),
     path('delete/', programa_delete, name='programa_delete'),
 ]
--- apps/programa/views.py
+++ apps/programa/views.py
@@ -1,15 +1,28 @@
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views import View
+from django.views.generic import ListView, UpdateView
 
 from .forms import ProgramaForm
 from .models import Programa
 
 
-def programa_lista(request):
-    programas = Programa.objects.all()
-    return render(request, 'programa/lista.html',
-                  {'programas': programas})
+# def programa_lista(request):
+#     programas = Programa.objects.all()
+#     return render(request, 'programa/lista.html',
+#                   {'programas': programas})
+
+
+class ProgramaListaView(ListView):
+    model = Programa
+    queryset = Programa.objects.all()
+    template_name = 'programa/lista.html'
+    context_object_name = 'programas'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(nombre__icontains='programa')
 
 
 def programa_detalle(request, pk):
@@ -19,21 +32,40 @@ def programa_detalle(request, pk):
                   {'programa': programa})
 
 
-def programa_create(request):
-    nuevo_programa = None
-    if request.method == 'POST':
-        programa_form = ProgramaForm(request.POST, request.FILES)
+# def programa_create(request):
+#     nuevo_programa = None
+#     if request.method == 'POST':
+#         programa_form = ProgramaForm(request.POST, request.FILES)
+#         if programa_form.is_valid():
+#             # Se guardan los datos que provienen del formulario en la B.D.
+#             nuevo_programa = programa_form.save(commit=True)
+#             messages.success(request,
+#                              'Se ha agregado correctamente el Programa {}'.format(nuevo_programa))
+#             return redirect(reverse('programa:programa_detalle', args={nuevo_programa.id}))
+#     else:
+#         programa_form = ProgramaForm()
+#
+#     return render(request, 'programa/programa_form.html',
+#                   {'form': programa_form})
+
+
+class ProgramaCreateView(View):
+    form_class = ProgramaForm
+    template_name = 'programa/programa_form.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        programa_form = self.form_class(request.POST, request.FILES)
         if programa_form.is_valid():
             # Se guardan los datos que provienen del formulario en la B.D.
             nuevo_programa = programa_form.save(commit=True)
             messages.success(request,
                              'Se ha agregado correctamente el Programa {}'.format(nuevo_programa))
             return redirect(reverse('programa:programa_detalle', args={nuevo_programa.id}))
-    else:
-        programa_form = ProgramaForm()
-
-    return render(request, 'programa/programa_form.html',
-                  {'form': programa_form})
+        return render(request, self.template_name, {'form': programa_form})
 
 
 def programa_delete(request):
@@ -48,15 +80,25 @@ def programa_delete(request):
     return redirect(reverse('programa:programa_lista'))
 
 
-def programa_edit(request, pk):
-    programa = get_object_or_404(Programa, pk=pk)
-    if request.method == 'POST':
-        form_programa = ProgramaForm(request.POST, request.FILES, instance=programa)
-        if form_programa.is_valid():
-            form_programa.save()
-            messages.success(request, 'Se ha actualizado correctamente el Programa')
-            return redirect(reverse('programa:programa_detalle', args=[programa.id]))
-    else:
-        form_programa = ProgramaForm(instance=programa)
+# def programa_edit(request, pk):
+#     programa = get_object_or_404(Programa, pk=pk)
+#     if request.method == 'POST':
+#         form_programa = ProgramaForm(request.POST, request.FILES, instance=programa)
+#         if form_programa.is_valid():
+#             form_programa.save()
+#             messages.success(request, 'Se ha actualizado correctamente el Programa')
+#             return redirect(reverse('programa:programa_detalle', args=[programa.id]))
+#     else:
+#         form_programa = ProgramaForm(instance=programa)
+#
+#     return render(request, 'programa/programa_edit.html', {'form': form_programa})
 
-    return render(request, 'programa/programa_edit.html', {'form': form_programa})
+
+class ProgramaUpdateView(UpdateView):
+    model = Programa
+    template_name = 'programa/programa_form.html'
+    form_class = ProgramaForm
+
+    def get_success_url(self):
+        messages.success(self.request, 'Se ha actualizado correctamente el Programa')
+        return reverse_lazy('programa:programa_lista')
